2b8e34647556a4cfd996f1d375c0df4ba067cf7b
Ricardo Oliveira <ricardo@hashicorp.com>
2024-02-19 23:58:56 +0000

    Update virtual environment setup and add new Streamlit script

--- run_embed.sh
+++ run_embed.sh
@@ -3,9 +3,13 @@
 
 
 ulimit -n 10240
-python3 -m venv env
+if [ -d "env" ]; then
+    echo "Directory 'env' already exists."
+else 
+    python3 -m venv env
+fi
 source env/bin/activate
-
+pip install --upgrade pip
 # export GITHUB_PERSONAL_ACCESS_TOKEN=
 pip install -r requirements.streamlit.txt
 python embed_hashicorp.py
--- run_streamlit.sh
+++ run_streamlit.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-python3 -m venv env
+if [ -d "env" ]; then
+    echo "Directory 'env' already exists."
+else 
+    python3 -m venv env
+fi
 source env/bin/activate
 
 # export GITHUB_PERSONAL_ACCESS_TOKEN=
-
+pip install --upgrade pip
 pip install -r requirements.streamlit.txt
 ollama pull mistral:7b
-python -m streamlit run streamlit.py
+python -m streamlit run streamlit_chat.py
