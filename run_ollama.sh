6ee2d1e9cef944ab011269b1d21df4b00697442f
Ricardo Oliveira <ricardo@hashicorp.com>
2024-02-22 23:05:11 +0000

    Update OLLAMA_HOST in run_ollama.sh script

--- run_ollama.sh
+++ run_ollama.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-export OLLAMA_HOST=http://0.0.0.0:11434
+# export OLLAMA_HOST=http://0.0.0.0:11434
+# launchctl setenv OLLAMA_HOST "0.0.0.0"
+
+export OLLAMA_HOST=0.0.0.0
 ollama serve
