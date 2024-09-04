0eefc160389907eb6303725352c9349139968c9b
Ricardo Oliveira <ricardo@hashicorp.com>
2024-09-04 21:56:06 +0100

    feat: Simplify prompt format
    feat: Update langchain_community.vectorstores.chroma import statement
    
    chore: Add environment variable for attention flash
    
    Refactor common.py to use langchain_ollama and langchain_chroma packages
    
    chore: Ignore "docs_skip" directory in .gitignore
    
    Updated dependencies
    
    Refactor get_model_list function to sort the model names before returning them
    
    Bump langchain-community from 0.2.1 to 0.2.9 in the pip group across 1 directory
    
    Bump langchain-community in the pip group across 1 directory

--- ai/hashi_prompts.py
+++ ai/hashi_prompts.py
@@ -1,82 +1,54 @@
 import logging
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 
-import ollama
-from langchain_core.prompts import PromptTemplate
-
-
-def prompt_from_model(model_name: str) -> str:
-    template = ollama.show(model_name)['template']\
-        .replace("{{ if .System }}", "")\
-        .replace("{{ if .Prompt }}", "")\
-        .replace("{{ end }}", "")\
-        .replace("{{- if .System }}", "")\
-        .replace("{{- end }}", "")\
-        .replace("{{ .Response }}<end_of_turn>", "")\
-        .replace("{{ .Response }}<|im_end|>", "")\
-        .replace("{{ .Response }}<|end|>", "")\
-        .replace("{{ .Response }}<|END_OF_TURN_TOKEN|>", "")\
-        .replace("{{ .Response }}", "")\
-        .replace("{{ .System }}", " {system} ")\
-        .replace("{{ .Prompt }}", " {prompt} ")
-        # .replace("{{ .Response }}<|eot_id|>", "")\
-        
-    if model_name.startswith("starling-lm"):
-        template = ollama.show(model_name)['template'].replace("{{ .System }}", "{system}").replace("{{ .Prompt}}", "{prompt} ")
-
-    logging.debug(f"Base template used: {template}")
-    return template
-
-
-# mistral_instruct_prompt_template = (
-#     "<s> [INST] You are a friendly assistant for question-answering tasks and an expert in HashiCorp technology. \n"
-#     "Use the following pieces of retrieved context to answer the question, together with the chat history and your own knowledge. \n"
-#     "If you don't know the answer, just say that you don't know or ask questions to clarify. Keep the answer concise, in markdown format, and always add external references to your source of knowledge. [/INST] </s> \n"
-    
-#     "[INST]Chat History: {chat_history} \n" 
-#     "Question: {question} \n"
-#     "Context: {context} \n"
-#     "Answer: [/INST]"
-# )
-
-def QA_prompt(model_name: str) -> PromptTemplate:
-    template = prompt_from_model(model_name=model_name)
-    prompt = template.format(
-        system=(
+def QA_prompt() -> ChatPromptTemplate:
+    messages = [
+        (
+            "system",
+            (
             "You are a friendly assistant for question-answering tasks and an expert in HashiCorp technology. \n"
             "All questions are in the context of HashiCorp products. \n"
             "Use the chat history and the retrieved context to answer the question. \n"
             "If you don't know the answer, just say that you don't know and ask the user to clarify. Keep the answer concise. Use markdown format. Provide external references for verification. \n"
-        ), prompt=(
+            )
+        ),
+        (
+            "user",
+            (
             "Chat History: {chat_history} \n" 
             "Question: {question} \n"
             "Context: {context} \n"
             "Answer:"
+            )
         )
-    )
+    ]
     
-    return PromptTemplate.from_template(prompt)
-                                        # partial_variables={'chat_history': ''})
+    return ChatPromptTemplate.from_messages(messages)
 
-def search_prompt(model_name: str) -> PromptTemplate:
-    template = prompt_from_model(model_name=model_name)
-    prompt = template.format(
-        system=(
-            "You are a friendly search assistant and an expert in HashiCorp technology. \n"
-            "All questions are in the context of HashiCorp products. \n"
-            "Use only the pieces of retrieved context to answer the question. \n"
-            "If you don't know the answer, just say you don't know. Keep the answer concise. Use markdown format. Provide external references for verification. \n"
-        ), prompt=(
-            "Question: {question} \n"
-            "Context: {context} \n"
-            "Answer:"
+
+def search_prompt() -> ChatPromptTemplate:
+    message = [
+        (
+            "system",
+            (
+                "You are a friendly search assistant and an expert in HashiCorp technology. \n"
+                "All questions are in the context of HashiCorp products. \n"
+                "Use only the pieces of retrieved context to answer the question. \n"
+                "If you don't know the answer, just say you don't know. Keep the answer concise. Use markdown format. Provide external references for verification. \n"
+            )
+        ),
+        (
+            "user",
+            (
+                "Question: {question} \n"
+                "Context: {context} \n"
+                "Answer:"
+            )
         )
-    )
+    ]
     
-    return PromptTemplate.from_template(prompt)
+    return ChatPromptTemplate.from_messages(message)
 
-
-# MISTRAL_INSTRUCT_PROMPT = PromptTemplate.from_template(mistral_instruct_prompt_template,
-#                                                        partial_variables={'chat_history': ''})
 
 
 mistral_instruct_agent_prompt_template = (
@@ -108,27 +80,16 @@ MISTRAL_INSTRUCT_AGENT_PROMPT = PromptTemplate.from_template(mistral_instruct_ag
                                                partial_variables={
                                                    'chat_history': '',})
 
-# INSTRUCTIONS = (
-#     "<s> [INST] You are a friendly assistant for question-answering tasks and an expert in HashiCorp technology. \n"
-#     "If you don't know the answer, just say that you don't know. Keep the answer concise, in markdown format and always add references to your source of knowledge. [/INST] </s> \n"
-# )
-# input_variables=['agent_scratchpad', 'input', 'tool_names', 'tools']
-# template='Answer the following questions as best you can. You have access to the following tools:\n\n{tools}\n\nUse the following format:\n\nQuestion: the input question you must answer\nThought: you should always think about what to do\nAction: the action to take, should be one of [{tool_names}]\nAction Input: the input to the action\nObservation: the result of the action\n... (this Thought/Action/Action Input/Observation can repeat N times)\nThought: I now know the final answer\nFinal Answer: the final answer to the original input question\n\nBegin!\n\nQuestion: {input}\nThought:{agent_scratchpad}'
-
-#    hub.pull("langchain-ai/react-agent-template"),
-# input_variables=['agent_scratchpad', 'input', 'instructions', 'tool_names', 'tools']
-# partial_variables={'chat_history': ''}
-# template='{instructions}\n\nTOOLS:\n------\n\nYou have access to the following tools:\n\n{tools}\n\nTo use a tool, please use the following format:\n\n```\nThought: Do I need to use a tool? Yes\nAction: the action to take, should be one of [{tool_names}]\nAction Input: the input to the action\nObservation: the result of the action\n```\n\nWhen you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:\n\n```\nThought: Do I need to use a tool? No\nFinal Answer: [your response here]\n```\n\nBegin!\n\nPrevious conversation history:\n{chat_history}\n\nNew input: {input}\n{agent_scratchpad}'
-
-def writer_prompt_template(model_name: str):
-    
-    template = prompt_from_model(model_name=model_name)
-    # template = ollama.show(model_name)['template'].replace("{{ .System }}", "{system}").replace("{{ .Prompt }}", "{prompt}")
-    
-    # if model_name.startswith("starling-lm"):
-    #     template = ollama.show(model_name)['template'].replace("{{ .System }}", "{system}").replace("{{ .Prompt}}", "{prompt} ")
-
-    return template.format(
-        system=("You are an assistant that proposes an alternative way of writing, using the same tone. ",
-        "Use the input text to {instruction}"), prompt="input text: {input}")
-    
+def writer_prompt_template() -> ChatPromptTemplate:
+    return ChatPromptTemplate.from_messages([
+        (
+            "system",
+            ("You are an assistant that proposes an alternative way of writing, maintaining the same tone. "
+            "Use the input text to {instruction}"),
+        ),
+        (
+            "user",
+            "Input text: {input}",
+        ),
+    ])
+        
